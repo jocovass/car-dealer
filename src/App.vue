da37ad5c4215a0d0dd3cd5058fdc0ad81821e425
JozsefVass <joco.udv@gmail.com>
2021-08-28 14:27:38 +0100

    Added Header component.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <!-- header component here -->
+    <Header />
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -8,25 +10,13 @@
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Header from "./components/Header.vue";
 
-#nav {
-  padding: 30px;
+export default {
+  name: "App",
+  components: { Header },
+};
+</script>
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<style lang="scss"></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="header">
+    <h1 class="logo">CarDealer<sup class="text-primary">UK</sup></h1>
+  </header>
+</template>
+
+<style lang="scss">
+@import "../styles/app.scss";
+
+.header {
+  padding: 1.1rem 0.8rem 0.8rem;
+  border-bottom: 1px solid $text-light;
+
+  .logo {
+    font-size: 1.5rem;
+    font-weight: 700;
+    margin-bottom: 0;
+
+    sup {
+      font-size: 0.9rem;
+    }
+  }
+}
+</style>
