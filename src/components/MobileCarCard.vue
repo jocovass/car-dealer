9f87482853b9442a99f68cd11ee87eb3ff63ca64
JozsefVass <joco.udv@gmail.com>
2021-08-30 08:20:40 +0100

    Added ProductListing component, rendering the card components using the json data.

--- src/components/MobileCarCard.vue
+++ src/components/MobileCarCard.vue
@@ -2,7 +2,10 @@
   <div class="card" style="border: none">
     <div class="card__img-slide">
       <carousel :perPage="3" :paginationEnabled="false">
-        <slide class="slide">
+        <slide class="slide" v-for="index in 5" :key="index">
+          <img :src="getImages" alt="Mercedes" />
+        </slide>
+        <!-- <slide class="slide">
           <img src="../assets/images/mercedes.jpg" alt="Mercedes" />
         </slide>
         <slide class="slide">
@@ -13,10 +16,7 @@
         </slide>
         <slide class="slide">
           <img src="../assets/images/mercedes.jpg" alt="Mercedes" />
-        </slide>
-        <slide class="slide">
-          <img src="../assets/images/mercedes.jpg" alt="Mercedes" />
-        </slide>
+        </slide> -->
       </carousel>
     </div>
     <div class="card__details">
@@ -24,15 +24,19 @@
         <b-row class="align-items-start">
           <b-col class="col-8">
             <div class="card__heading">
-              <h3 class="card__title">2014 (64) Mercedes-Benz</h3>
-              <p class="card__subtitle">CLA 250e Coupe Shooting Break</p>
+              <h3 class="card__title">
+                {{ car.year ? car.year : null }}
+                {{ car.make ? car.make : null }}
+                {{ car.model ? car.model : null }}
+              </h3>
+              <p class="card__subtitle">{{ car.name }}</p>
             </div>
           </b-col>
           <b-col class="d-flex align-items-center">
             <div class="card__tags">
-              <b-badge class="font-weight-normal condition-badge mr-3"
-                >New</b-badge
-              >
+              <b-badge class="font-weight-normal condition-badge mr-3">{{
+                car.advert_classification
+              }}</b-badge>
               <b-icon icon="star" aria-hidden="true" :font-scale="1.3"></b-icon>
             </div>
           </b-col>
@@ -43,23 +47,27 @@
         <b-row>
           <b-col>
             <div class="card__specs">
-              <span class="card__spec">32 miles</span>
+              <span class="card__spec"
+                >{{ formatMiles }} {{ car.odometer_units }}</span
+              >
               <span class="card__spec--divider"> | </span>
-              <span class="card__spec">Hybrid</span>
+              <span class="card__spec">{{ car.transmission }}</span>
               <span class="card__spec--divider"> | </span>
-              <span class="card__spec">Manual</span>
+              <span class="card__spec">{{ car.fuel_type }}</span>
               <span class="card__spec--divider"> | </span>
-              <span class="card__spec">SUV</span>
+              <span class="card__spec">{{ car.body_type }}</span>
             </div>
           </b-col>
           <b-col>
             <div class="card__pricing">
               <div class="card__pricing--monthly">
-                <p><span>$550.90</span> /mo (PCP)</p>
+                <p v-show="!!car.monthly_payment">
+                  <span>{{ car.monthly_payment }}</span> /mo (PCP)
+                </p>
               </div>
               <div class="card__pricing--full d-flex">
-                <p class="card__pricing--new mr-2">$23,300</p>
-                <p class="card__pricing--old">$23,300</p>
+                <p class="card__pricing--new mr-2">${{ car.original_price }}</p>
+                <p class="card__pricing--old">${{ car.price }}</p>
               </div>
             </div>
           </b-col>
@@ -71,10 +79,44 @@
 
 <script>
 import { Carousel, Slide } from "vue-carousel";
+import mercedes from "@/assets/images/mercedes.jpg";
+import peugeot from "@/assets/images/peugeot.jpg";
+import renault from "@/assets/images/renault.jpg";
 
 export default {
   name: "MobileCarCard",
   components: { Carousel, Slide },
+  data() {
+    return {
+      images: {
+        mercedes,
+        peugeot,
+        renault,
+      },
+    };
+  },
+  props: {
+    car: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  computed: {
+    formatMiles() {
+      if (this.car.odometer_value < 1000) return this.car.odometer_value;
+
+      const formated = this.car.odometer_value / 1000;
+
+      return String(formated).split(".")[0] + "k";
+    },
+
+    getImages() {
+      if (this.images[this.car.make.toLowerCase()]) {
+        return this.images[this.car.make.toLowerCase()];
+      }
+      return Object.entries(this.images)[Math.floor(Math.random() * 3)][1];
+    },
+  },
 };
 </script>
 
@@ -93,6 +135,7 @@ export default {
 }
 
 .card {
+  margin-bottom: 3rem;
   &__title {
     font-size: 0.9rem;
     font-family: $font-family-base;
--- src/views/ProductListing.vue
+++ src/views/ProductListing.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <b-container>
-      <MobileCarCard />
+      <MobileCarCard v-for="car in cars" :key="car.stock_id" :car="car" />
     </b-container>
   </div>
 </template>
@@ -9,11 +9,32 @@
 <script>
 // @ is an alias to /src
 import MobileCarCard from "../components/MobileCarCard.vue";
+import json from "../data/vehicle-search.json";
 
 export default {
   name: "ProductListing",
   components: {
     MobileCarCard,
   },
+  data() {
+    return {
+      currentPage: 2,
+      itemPerPage: 6,
+      cars: [],
+    };
+  },
+  created() {
+    const start = (this.currentPage - 1) * 6;
+    this.cars = json.data.slice(start, start + this.itemPerPage);
+  },
 };
 </script>
+<!--
+  body_type, colour, feature_classification[0], make, model, odometer_value, original_price, price, model_year, monthly_payment, stock_id, transmission, year, advert_classification
+
+  year, make model   advert_classification, star
+  name
+
+  odometer_value transmission fuel_type body_type    monthly_payment
+                                                      original_price price
+ -->
